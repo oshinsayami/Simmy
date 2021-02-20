156b67019e7779a1f10292950cbf42aabbee5a3c
Simran <mdrsimran@gmail.com>
2021-02-19 19:19:11 -0600

    users controller codes

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,43 @@
 class UsersController < ApplicationController
+    def create
+        user = User.find_or_create_by(email: params[:email])
+        user.name = params[:name]
+        user.save
+
+        if user.carts.length == 0
+            cart = Cart.create(user_id: user.id)
+            user.carts << cart
+        end
+
+        render json: user, :include => {
+            carts: {
+                except: [:created_at, :updated_at],
+                methods: :total,
+                include: {
+                    cart_products:{
+                        inlcude: :product
+                    }
+                },
+            },
+        }, except: [:created_at, :updated_at]
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user, include => {
+            carts: {
+                except: [:created_at, :updated_at],
+                methods: :total,
+                include: {
+                    cart_products:{
+                        inlcude: :product
+                    }
+                },
+            },
+        }, except: [:created_at, :updated_at]
+        else
+            render json: {message: "User not found."}
+    end
+
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :carts
     has_many :cart_products
-    has_many :product, through: :cart_products
+    has_many :products, through: :cart_products
 end
