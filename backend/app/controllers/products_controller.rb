44664abfaec279b90c75cc0bfc6efbed2c2dca35
Simran <mdrsimran@gmail.com>
2021-02-19 19:30:28 -0600

    edit code in products controller

--- backend/app/controllers/products_controller.rb
+++ backend/app/controllers/products_controller.rb
@@ -4,4 +4,13 @@ class ProductsController < ApplicationController
         render json: ProductSerializer.new(products)
     end
 
+    def show
+        product = Product.find_by(id:params[:id])
+        if product
+            render json: products, except: [:created_at, :updated_at]
+        else 
+            render json: {message: "Product not found."}
+        end 
+    end 
+
 end
